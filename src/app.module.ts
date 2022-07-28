dc2fede511cf54db5018c870ec83059adbecbbbe
dunghenry <trandungksnb6930082001@gmail.com>
2022-07-28 10:26:07 +0700

    fix cors and .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { HttpModule, Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CarModule } from "./car/car.module";
 import { MongooseModule } from "@nestjs/mongoose";
+require("dotenv").config({ path: ".env" });
 @Module({
     imports: [
         MongooseModule.forRoot(
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ import { join } from "path";
 import hbs = require("hbs");
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.enableCors();
     app.useStaticAssets(join(__dirname, "..", "public"));
     app.setBaseViewsDir(join(__dirname, "..", "views"));
     app.setViewEngine("hbs");
     hbs.registerPartials(join(__dirname, "..", "views", "partials/"));
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
     console.log(`Server listening on http://localhost:3000`);
 }
 bootstrap();
